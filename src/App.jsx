7bb96dfcb88f56a7e05cb4cf6b8ae923906af452
lezyCoder <iamvijaysingh23@gmail.com>
2025-12-03 01:30:58 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './sections/HeroSection'
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+
+      <section>
+        <HeroSection />
+      </section>
+
+    </main>
+  )
+}
+
+export default App
